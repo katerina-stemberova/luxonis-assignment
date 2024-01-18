6a6b077f8eaed66d3a62711e95f7db29d6c8494f
Katerina Stemberova <katerina.stemberova@gmail.com>
2024-01-18 01:39:05 +0100

    scraping is run from a script
    i.e., not via using 'scrapy crawl flats' from the command line

---
+++ scrapy/sreality/scrape.py
@@ -0,0 +1,16 @@
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+from sreality.spiders.flats import FlatsSpider
+
+# Get the project settings
+settings = get_project_settings()
+
+# Create a CrawlerProcess with project settings
+process = CrawlerProcess(settings)
+
+# Add the spider to the process
+process.crawl(FlatsSpider)
+
+# Start the crawling process
+process.start()
